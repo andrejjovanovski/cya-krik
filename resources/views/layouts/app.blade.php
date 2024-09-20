65dd1c511d47b91b592afe4a988477f75dc95e3c
Ilija Ristov <ilijaristov67@gmail.com>
2024-09-20 11:43:16 +0100

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,32 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.google.com/specimen/Mulish?query=mulish">
     <link href="https://fonts.google.com/specimen/Montserrat?query=mont">
+
     @vite('resources/css/app.css')
     <title>@yield('title', 'Default Title')</title>
 </head>
+
 <body>
-{{--NEWSLETTER HEADER BAR--}}
+    {{--NEWSLETTER HEADER BAR--}}
+    @include('layouts.newsletter_bar')
+    {{--NAVBAR--}}
 
-{{--NAVBAR--}}
+    {{--MAIN CONTENT--}}
+    <div class="container">
+        @yield('content')
+    </div>
 
-{{--MAIN CONTENT--}}
-<div class="container">
-    @yield('content')
-</div>
-
-{{--FOOTER--}}
-@include('layouts.footer')
+    {{--FOOTER--}}
+    @include('layouts.footer')
+    <!-- <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script> -->
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
   theme: {
     extend: {
-        fontFamily: {
-            sans: ['Montserrat', 'sans-serif'],
-            heading: ['Mulish', 'sans-serif']
-        },
-        colors: {
-            orange: '#FBB13C',
-            black: '#191919',
-            lightGray: '#CAC6C6',
-            whiteBackground: '#FBF8F4',
-            purple: '#B8AFDA',
-            red: '#FB5E3C',
-        }
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        heading: ['Mulish', 'sans-serif']
+      },
+      colors: {
+        orange: '#FBB13C',
+        black: '#191919',
+        lightGray: '#CAC6C6',
+        whiteBackground: '#FBF8F4',
+        purple: '#B8AFDA',
+        red: '#FB5E3C',
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
