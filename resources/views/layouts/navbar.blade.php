2edfd39b41dc48cf1080cdc8b4ce32d5a77cdc86
Ilija Ristov <ilijaristov67@gmail.com>
2024-09-21 10:45:30 +0100

    finished_navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,32 +1,48 @@
-<div class="pt-8 lg:pt-16 md:pt-16 sm:pt-10 pb-1 bg-orange w-full fixed z-40">
+<div class="pt-8 lg:pt-16 md:pt-16 sm:pt-10 m-auto pb-1 bg-orange w-full fixed z-40">
     <div class="relative flex items-center justify-between w-3/4 m-auto">
         <img id="logoImg" class="h-16 w-16" src="{{ asset('images/Крик-лого.png') }}" alt="Main Logo">
-        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black-500 rounded-lg md:hidden hover:bg-black-100 focus:outline-none dark:text-black-400 dark:hover:bg-black-700 dark:focus:ring-black-600 absolute right-0 top-0 mt-3 mr-3" aria-controls="navbar-default" aria-expanded="false">
+        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black-500 rounded-lg md:hidden hover:bg-black-100 focus:outline-none   absolute right-0 top-0 mt-3 mr-3" aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path class="path" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
         </button>
-
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="text-black flex flex-col p-4 md:p-0 md:flex-row md:space-x-2 rtl:space-x-reverse md:mt-0">
+            <ul class="text-black flex flex-col p-4 md:p-0 md:flex-row md:space-x-4 rtl:space-x-reverse md:mt-0 items-center">
                 <img id="logoImg2" class="h-16 w-16 hidden" src="{{ asset('images/Крик-лого.png') }}" alt="Secondary Logo">
-                <li>
-                    <a href="#" class="font-semibold text-lg block py-2 px-4">За нас</a>
+                <li class="relative font-medium text-lg flex items-center">
+                    <div class="dropdown-container inline-block">
+                        <div class="dropdown-container">
+                            <button id="dropdownBtn" class="font-semibold bg-orange text-black text-2xl px-3 cursor-pointer flex items-center justify-center">
+                                За нас
+                                <i id="arrow" class="arrow down ml-2 mb-2 text-white"></i> <!-- The arrow becomes white -->
+                            </button>
+
+                            <ul id="customDropdown" class="hidden absolute bg-black text-white text-center">
+                                <li class="px-3 cursor-pointer">За Крик</li>
+                                <li class="px-3 cursor-pointer">Нашиот Тим</li>
+                                <li class="px-3 cursor-pointer">Волонтери</li>
+                                <li class="px-3 cursor-pointer">Архива</li>
+                            </ul>
+                        </div>
                 </li>
-                <li>
-                    <a href="#" class="font-semibold text-lg block py-2 px-4">Услуги</a>
+                <li class="li-elements-css mt-1">
+                    <a href="#" class="li-elements font-semibold text-2xl text-lg py-2 px-3 rounded-full">Услуги</a>
                 </li>
-                <li>
-                    <a href="#" class="font-semibold text-lg block py-2 px-4">Проекти</a>
+                <li class="li-elements-css mt-2">
+                    <a href="#" class="li-elements font-semibold text-2xl text-lg py-2 px-3 rounded-full">Проекти</a>
                 </li>
-                <li>
-                    <a href="#" class="font-semibold text-lg block py-2 px-4">Контакт</a>
+                <li class="li-elements-css mt-2">
+                    <a href="#" class="li-elements font-semibold text-2xl text-lg py-2 px-3 rounded-full">Контакт</a>
                 </li>
-                <li>
-                    <a href="#" class="block py-2 px-4 text-white bg-black rounded-full font-thin">Донирај</a>
+                <li class="mt-2">
+                    <a id="donate" href="#" class=" py-2 font-semibold px-3 text-white bg-black rounded-t-full rounded-full text-xl hover:bg-purple">
+                        Донирај
+                    </a>
                 </li>
+
             </ul>
         </div>
     </div>
 </div>
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur quos voluptatem explicabo consequuntur, saepe dolor aperiam molestiae modi a harum. At saepe ex cupiditate fuga, totam possimus nihil sunt! Libero facere ex beatae ab quis est a incidunt perferendis rem animi fuga, suscipit et dolorum nobis nostrum, tempora eius illum officia quod. Quos, possimus? Non sapiente suscipit ducimus architecto id rerum saepe esse repellat accusamus culpa vel consectetur nulla excepturi maiores nemo, facere, nesciunt expedita libero eligendi mollitia iusto nostrum debitis eaque! Est error tempora ea aspernatur eaque quia consequatur voluptates minus eligendi. Quam vero deleniti maxime, autem maiores, distinctio veritatis fugiat hic sint, qui nemo deserunt magnam! Iure doloribus maxime pariatur quidem soluta asperiores eos nesciunt, dolorem, ad at expedita facilis architecto alias porro fugit molestiae illum. Architecto, alias? Libero qui aspernatur modi, non voluptatibus recusandae. Reprehenderit nesciunt autem voluptate veniam, rerum eligendi doloremque mollitia esse provident error sequi consequuntur minus, illo officiis officia repellat, iure numquam cumque aliquam? Quos dicta fugit voluptas quis rem eos distinctio eum cumque accusantium soluta saepe dignissimos, ut praesentium beatae inventore recusandae itaque? Alias maxime a aspernatur iure in distinctio facilis totam saepe praesentium, molestias omnis asperiores reiciendis, illum libero ad dignissimos iste eveniet delectus sequi, magni nulla ipsam. Cupiditate impedit velit soluta aliquid? Itaque dolorum dignissimos optio ad alias quaerat? Eos, mollitia repellat amet doloremque dolores dicta iure ex sint molestiae in, tempora officiis? Facilis vitae sed deserunt neque tempore corrupti deleniti ipsa quis libero, beatae iste quam veritatis excepturi doloremque non, impedit repudiandae quisquam asperiores corporis dolores. Amet assumenda labore repellendus incidunt perferendis modi sequi quo necessitatibus laboriosam! Ad ea eum ratione repudiandae sunt adipisci quis pariatur repellendus totam modi vero rerum, dolore officiis reiciendis, minima sed commodi eos omnis labore cumque neque odit numquam alias. Minima dolores aut excepturi cum dolor ut dolorum facere temporibus, magnam fugit quos ipsam quaerat, mollitia voluptate quod autem vel inventore harum itaque? Delectus voluptates enim consequatur. Animi, omnis laborum libero dolorum facere voluptatem, aut tempore iure culpa repellat magnam. Nobis neque corrupti esse placeat optio fugiat amet autem cupiditate libero necessitatibus expedita quasi accusamus quaerat, asperiores non labore cumque alias quisquam voluptates esse minus, ducimus facilis consectetur ipsa quae maxime accusantium similique. Veritatis iusto tempora debitis ducimus nemo cupiditate optio animi? Commodi ut aspernatur voluptatibus id sint perspiciatis quis pariatur impedit debitis libero dicta, repudiandae quam sunt suscipit fuga ipsa, ab beatae repellat ipsum assumenda odit? Possimus officia quidem iusto voluptatem delectus sed, in nostrum facilis totam quia cumque quos tempore nemo nihil unde sunt maiores dicta, quo earum quaerat. Mollitia aperiam culpa saepe quam suscipit ea, voluptatibus ipsum nihil repudiandae voluptas laborum amet? Saepe, voluptatum minus in vero a repellat accusamus quasi sed doloremque alias dolorem vitae sunt et inventore rerum deleniti quidem adipisci? Ea, ullam distinctio. Odio voluptatum repudiandae cupiditate! Minus repellendus enim eligendi tempore necessitatibus sed sint voluptates exercitationem, doloribus alias fugiat consectetur asperiores! Odit, commodi. Minima maxime voluptate id autem veritatis perspiciatis vitae laborum ducimus quisquam. Nesciunt neque laudantium quis vel omnis incidunt quaerat temporibus fugiat amet deleniti, provident, velit aperiam repellendus voluptates veritatis sequi laboriosam molestiae sunt, distinctio numquam. Itaque culpa aliquid qui nulla? Non laboriosam iusto, exercitationem neque quae sit animi ipsam dolore, optio in eos? Iure minima animi hic quidem! Laudantium distinctio veniam fuga exercitationem voluptatem! Blanditiis eum nam amet iusto pariatur nulla porro optio earum, nobis eaque excepturi, at, adipisci consequatur sapiente enim impedit rem! Placeat, voluptatem animi, culpa dolores ex vero earum aspernatur et, quibusdam exercitationem repellendus laboriosam recusandae quis incidunt sint ea eius asperiores? Qui fugit similique, earum consequuntur voluptate odio itaque. Optio, sunt!
