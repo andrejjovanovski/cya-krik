8cb45412eadff34b333f9eb78c20e1a8a642385f
Andrej Jovanovski <andrejjovanovski17@gmail.com>
2024-09-21 16:37:11 +0200

    project showcase page styles

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,6 +14,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
+
         return view('eshop.index', compact('products'));
 
     }
