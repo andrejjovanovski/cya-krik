b4645a12f195be54a86451d4393950a9adcdc807
Jovan Maksimoski <jovanmaksimoskii@gmail.com>
2024-09-21 11:59:35 +0200

    finished video section and currently working on news with slider

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -80,6 +80,60 @@
         </div>
 
     </section>
+    <hr class=" h-0.5 bg-black ">
+
+    {{--    vide section and стани волонтер  --}}
+
+    <section class="flex justify-center content-center p-10">
+        <div class="container  justify-center ml-auto grid grid-cols-1 md:grid-cols-2 gap-8 ">
+            <div class="shadow-lg p-8 rounded-xl" >
+                <video class="w-full rounded-lg" controls>
+                    <source src="" type="video/mp4">
+                    video
+                </video>
+            </div>
+
+
+            <div class="flex flex-col justify-between w-2/4  shadow-lg p-8 bg-black rounded-3xl" >
+                <h3 class="text-4xl font-bold mb-4 text-white">Стани волонтер</h3>
+                <p class="text-lg mb-6 capitalize text-lightGray">сакаш да работиш со млади лица? оваа можност е токму за тебе.</p>
+                <div class="flex">
+                    <button class="inline-block border-2 border-white text-white py-2 px-3 rounded-3xl capitalize text-center font-bold transition duration-300 ease-in-out hover:bg-white hover:text-black"><a href="" >придружи ни се</a></button>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    {{--    news with slider --}}
+
+
+
+    <section>
+
+
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+
+                <img class="rounded-t-lg" src="{{ asset('/public/images/Homepage/homepage_card_2.jpg') }}" alt="" />
+
+            </a>
+            <div class="p-5">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+
+    </section>
 
 
 @endsection
