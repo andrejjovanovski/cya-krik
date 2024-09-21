8b432e391adf638984b14a2fa6321dbce7fe71b7
Andrej Jovanovski <andrejjovanovski17@gmail.com>
2024-09-21 17:39:29 +0200

    blade and routes cleanup

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/footer', function () {
-    return view('test');
-});
-
 Route::resource('product', ProductController::class)->names('products');
 
 Route::get('/about', function () {
